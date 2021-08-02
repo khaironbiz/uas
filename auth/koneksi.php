2b9d770b3374f6570be85586ac1e1145195f8d5d
khaironbiz <khaironbiz@gmail.com>
2021-08-03 04:21:33 +0700

    pembetulan koneksi

--- auth/koneksi.php
+++ auth/koneksi.php
@@ -1,5 +1,8 @@
 <?php 
- $host = mysqli_connect("localhost", "root", "", "ak");
+//koneksi ke local
+$host = mysqli_connect("localhost", "root", "", "ak");
+//koneksi ke hosting
+//$host = mysqli_connect("localhost", "u853814933_uas", "Sql250909", "u853814933_uas");
 
  if($host){
   
